8666332e87fdb3940ee03603fc1946fba902b5b9
zhangtingwz <948551971@qq.com>
2018-06-02 17:44:50 +0800

    2nd

--- Project1/Project1/text.cpp
+++ Project1/Project1/text.cpp
@@ -1,6 +1,7 @@
 
 #include "highgui.h";
 #include "cv.h";
+
 /*加载图片*/
 /*
 int main(int argc, char** argv) {
@@ -222,34 +223,174 @@ int main(int argc, char** argv) {
 //
 //}
 /*鼠标回调事件*/
-void my_mouse_callback(
-	int event, int x, int y, int flags, void * param
-)
-CvRect box;
-bool drawing_box = false;
+//void my_mouse_callback(
+//	int event, int x, int y, int flags, void * param
+//)
+//CvRect box;
+//bool drawing_box = false;
+//
+//void draw_box(IplImage*img, CvRect rect) {
+//	cvRectangle(
+//		img,
+//		cvPoint(box.x, box.y),
+//		cvPoint(box.x + box.width,box.y+box.height),
+//		cvScalar(0xff, 0x00, 0x00)
+//	);
+//}
+//int main(int argc, char* argv[]) {
+//	box = cvRect(-1, -1, 0, 0);
+//	IplImage*image = cvCreateImage(
+//		cvSize(200, 200),
+//		IPL_DEPTH_8U,
+//		3
+//	);
+//	cvZero(image);//这个函数会将数组中的所有通道的元素的值都设置为0.
+//	IplImage*temp = cvCloneImage(image);//函数会自己开一段内存，然后复制好image里面的数据，然后把这段内存中的数据返回
+//	cvNamedWindow("Box Example");
+//	cvSetMouseCallback("Box Example", my_mouse_callback, (void*)image);
+//
+//	while (1) {
+//		cvCopy(image, temp);
+//		if (drawing_box) draw_box(temp, box);
+//		cvShowImage("Box Example", temp);
+//		if (cvWaitKey(15) == 27)break;
+//	}
+//
+//	cvReleaseImage(&image);
+//	cvReleaseImage(&temp);
+//	cvDestroyWindow("Box Example");
+//}
+//
+//void my_mouse_callback(int event,int x,int y,int flags,void*param) {
+//	IplImage* image = (IplImage*)param;
+//	switch (event)
+//	{
+//	case CV_EVENT_MOUSEMOVE: {
+//		if (drawing_box) {
+//			box.width = x - box.x;
+//			box.height = y - box.y;
+//		}
+//	}break;
+//	case CV_EVENT_LBUTTONDOWN: {
+//		drawing_box = true;
+//		box = cvRect(x, y, 0, 0);
+//	}break;
+//	case CV_EVENT_LBUTTONUP: {
+//		drawing_box = false;
+//		if (box.width < 0) {
+//			box.x += box.width;
+//			box.width *= -1;
+//		}
+//		if (box.height < 0) {
+//			box.y += box.height;
+//			box.height *= -1;
+//		}
+//		draw_box(image, box);
+//		
+//	}break;
+//	default:
+//		break;
+//	}
+//}
 
-void draw_box(IplImage*img, CvRect rect) {
-	cvRectangle(
-		img,
-		cvPoint(box.x, box.y),
-		cvPoint(box.x + box.width, box.y + box.height),
-		cvScalar(0xff, 0x00, 0x00)
-	);
-}
-int main(int argc, char* argv[]) {
-	box = cvRect(-1, -1, 0, 0);
-	IplImage*image = cvCreateImage(
-		cvSize(200, 200),
-		IPL_DEPTH_8U,
-		3
-	);
-	cvZero(image);//这个函数会将数组中的所有通道的元素的值都设置为0.
-	IplImage*temp = cvCloneImage(image);//函数会自己开一段内存，然后复制好image里面的数据，然后把这段内存中的数据返回
-	cvNamedWindow("Box Example");
-	cvSetMouseCallback("Box Example", my_mouse_callback, (void*)image);
+//void main() {
+//	IplImage *src, *dst_blur, *dst_median, *dst_gaussian,*dst_Eorde, *dst_Dilate;
+//	src = cvLoadImage("rice.jpg",1);//好像必须灰度图
+//	dst_blur = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+//	dst_median = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+//	dst_gaussian = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+//	dst_Eorde = cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+//	dst_Dilate= cvCreateImage(cvGetSize(src), IPL_DEPTH_8U, 1);
+//	cvNamedWindow("src", 1);
+//	cvNamedWindow("blur", 1);
+//	cvNamedWindow("median", 1);
+//	cvNamedWindow("gaussian", 1);
+//	cvNamedWindow("Eorde", 1);
+//	cvNamedWindow("Dilate", 1);
+//
+//	IplConvKernel * x = cvCreateStructuringElementEx(
+//		1, 2, 0, 0, CV_SHAPE_CROSS
+//	);
+//
+//	cvSmooth(src, dst_blur, CV_BLUR, 3, 3, 0, 0);         //邻域平均滤波  
+//	//cvSmooth(src, dst_median, CV_MEDIAN, 3, 3, 0, 0);     //中值滤波   
+//	//cvSmooth(src, dst_gaussian, CV_GAUSSIAN, 3, 3, 0, 0); //高斯滤波  
+//	
+//	//cvErode(src, dst_Eorde);
+//	//cvDilate(src, dst_Dilate);
+//	//cvShowImage("src", src);
+//	cvShowImage("blur", dst_blur);
+//	cvShowImage("median", dst_median);
+//	cvShowImage("gaussian", dst_gaussian);
+//	cvShowImage("Eorde", dst_Eorde);
+//	cvShowImage("Dilate", dst_Dilate);
+//	cvWaitKey(0);
+//	cvReleaseImage(&src);
+//	cvReleaseImage(&dst_blur);
+//	cvReleaseImage(&dst_median);
+//	cvReleaseImage(&dst_gaussian);
+//
+//	return ;
+//}
+/*人脸识别*/
 
-	while (1) {
-		cvCpoyImage(image, temp);
-	}
-}
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+	
+		Mat image, image_gray;      //定义两个Mat变量，用于存储每一帧的图像
+
+		image = imread("2.jpeg");
+		cvtColor(image, image_gray, CV_BGR2GRAY);//转为灰度图
+		equalizeHist(image_gray, image_gray);//直方图均衡化，增加对比度方便处理
+
+		CascadeClassifier eye_Classifier;  //载入分类器
+		CascadeClassifier face_cascade;
+
+		string cascadeName = "haarcascade_eye.xml";
+		eye_Classifier.load(cascadeName);
+		//if (!eye_Classifier.load("haarcascade_eye.xml"))  //需要将xml文档放在自己指定的路径下
+		//{
+		//	cout << "Load haarcascade_eye.xml failed!" << endl;
+		//	return 0;
+		//}
+
+		if (!face_cascade.load("haarcascade_frontalcatface.xml"))
+		{
+			cout << "Load haarcascade_frontalface_alt failed!" << endl;
+			return 0;
+		}
+
+		//vector 是个类模板 需要提供明确的模板实参 vector<Rect>则是个确定的类 模板的实例化
+		vector<Rect> eyeRect;
+		vector<Rect> faceRect;
+
+		//检测关于眼睛部位位置
+		eye_Classifier.detectMultiScale(image_gray, eyeRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+		for (size_t eyeIdx = 0; eyeIdx < eyeRect.size(); eyeIdx++)
+		{
+			rectangle(image, eyeRect[eyeIdx], Scalar(0, 0, 255));   //用矩形画出检测到的位置
+		}
+
+		//检测关于脸部位置
+		face_cascade.detectMultiScale(image_gray, faceRect, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+		for (size_t i = 0; i < faceRect.size(); i++)
+		{
+			rectangle(image, faceRect[i], Scalar(0, 0, 255));      //用矩形画出检测到的位置
+		}
+
+		imshow("人脸识别图", image);         //显示当前帧
+		waitKey(0);
+		return 0;
+}              
